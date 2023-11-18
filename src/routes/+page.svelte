550b3ce929bd88282ed1c83ac6b28de43ee410a1
Brekig <42963322+Brekig@users.noreply.github.com>
2023-11-18 13:32:55 +0000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+
+
+//  $: document.title = `Clicked ${button} times`;
+</script>
+
+
+<section>
+    <div class="outer">
+            
+        <h1>Neglur Katla</h1>
+        <p>Velkomin a heimasidu neglur katla</p>
+    </div>
+
+</section>
+    
+
+<style>
+.outer {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	align-self: center;
+	margin: auto;
+	min-height: 30em;
+	max-width: 60em;
+}
+
+@media (max-width: 1000px) {
+	.outer {
+		flex-direction: column;
+		align-items: center;
+		min-height: 30em;
+	}
+}
+</style>
